3e32d11fed81a517e3378fd0e730f433a5029c95
ammarbahtiarasli <ammarbahtiarasli@gmail.com>
2023-11-03 02:40:28 +0700

    wip #13

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,15 +1,39 @@
 @extends('layouts.main')
 
 @section('container')
-<h1 class="mb-5">{{ $title }}</h1>
-      @if ($posts->count())
+    <h1 class="mb-3 text-center">{{ $title }}</h1>
+
+    <div class="row justify-content-center mb-3">
+        <div class="col-md-6">
+            <form action="/posts">
+                @if (request('category'))
+                    <input type="hidden" name="category" value="{{ request('category') }}">
+                @endif
+                @if (request('author'))
+                    <input type="hidden" name="author" value="{{ request('author') }}">
+                @endif
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Search..." name="search"
+                        value="{{ request('search') }}">
+                    <button class="btn btn-outline-danger" type="submit">Search</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    @if ($posts->count())
         <div class="card mb-3">
-            <img src="https://picsum.photos/1200/400?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
+            <img src="https://picsum.photos/1200/400?{{ $posts[0]->category->name }}" class="card-img-top"
+                alt="{{ $posts[0]->category->name }}">
             <div class="card-body text-center">
-                <h5 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h5>
+                <h5 class="card-title"><a href="/posts/{{ $posts[0]->slug }}"
+                        class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h5>
                 <p>
                     <small class="text-body-secondary">
-                        By. <a href="/authors/{{ $posts[0]->author->username }}" class="text-secondary text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/categories/{{ $posts[0]->category->slug }}" class="text-info-emphasis text-decoration-none">{{ $posts[0]->category->name }}</a>
+                        By. <a href="/posts?category={{ $posts[0]->author->username }}"
+                            class="text-secondary text-decoration-none">{{ $posts[0]->author->name }}</a> in <a
+                            href="/posts?category={{ $posts[0]->category->slug }}"
+                            class="text-info-emphasis text-decoration-none">{{ $posts[0]->category->name }}</a>
                         {{ $posts[0]->created_at->diffForHumans() }}
                     </small>
                 </p>
@@ -17,31 +41,39 @@
                 <a href="/posts/{{ $posts[0]->slug }}" class="btn btn-primary text-decoration-none">Read more</a>
             </div>
         </div>
-    @else
-        <p class="text-center fs-4 text-danger">No post found.</p>
-    @endif
 
-    <div class="container">
-        <div class="row">
-            @foreach ($posts->skip(1) as $post)
-                <div class="col-md-4">
-                    <div class="card">
-                        <div class="position-absolute px-3 py-2 text-white" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a></div>
-                        <img src="https://picsum.photos/500/400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $post->title }}</h5>
-                            <p>
-                                <small class="text-body-secondary">
-                                    By. <a href="/authors/{{ $post->author->username }}" class="text-secondary text-decoration-none">{{ $post->author->name }}</a>
-                                    {{ $post->created_at->diffForHumans() }}
-                                </small>
-                            </p>
-                            <p class="card-text">{{ $post->excerpt }}</p>
-                            <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read more</a>
+        <div class="container">
+            <div class="row">
+                @foreach ($posts->skip(1) as $post)
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="position-absolute px-3 py-2 text-white"
+                                style="background-color: rgba(0, 0, 0, 0.7)"><a href="/posts?category={{ $post->slug }}"
+                                    class="text-decoration-none text-white">{{ $post->category->name }}</a></div>
+                            <img src="https://picsum.photos/500/400?{{ $post->category->name }}" class="card-img-top"
+                                alt="{{ $post->category->name }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $post->title }}</h5>
+                                <p>
+                                    <small class="text-body-secondary">
+                                        By. <a href="/posts?author={{ $post->author->username }}"
+                                            class="text-secondary text-decoration-none">{{ $post->author->name }}</a>
+                                        {{ $post->created_at->diffForHumans() }}
+                                    </small>
+                                </p>
+                                <p class="card-text">{{ $post->excerpt }}</p>
+                                <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read more</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
+    @else
+        <p class="text-center fs-4">No post found.</p>
+    @endif
+
+    <div class="d-flex justify-content-end">
+        {{ $posts->links() }}
     </div>
 @endsection
