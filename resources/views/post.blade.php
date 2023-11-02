4f3c33110c155e6bb1886147b99fe66e180fe71d
ammarbahtiarasli <ammarbahtiarasli@gmail.com>
2023-11-03 02:28:20 +0700

    wip #12

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,22 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-5">{{ $post->title }}</h1>
-    <article>
-        <h2>{{ $post->title }}</h2>
 
-        <p>By. <a href="/authors/{{ $post->author->username }}" class="text-primary text-decoration-none">{{ $post->author->name }}</a> in <a
-                href="/categories/{{ $post->category->slug }}"
-                class="text-primary-emphasis text-decoration-none">{{ $post->category->name }}</a></p>
+<div class="container">
+    <div class="row justify-content-center mb-5">
+            <div class="col-md-8">
+                    <h1 class="mb-3">{{ $post->title }}</h1>
 
-        {!! $post->body !!}
-        <a class="text-primary-emphasis d-block mt-5" href="/posts">Back to Article</a>
-    </article>
+                    <p>By. <a href="/authors/{{ $post->author->username }}" class="text-secondary text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-info-emphasis text-decoration-none">{{ $post->category->name }}</a></p>
+
+                    <img src="https://picsum.photos/1200/400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+
+                    <article class="my-3 fs-5">
+                            {!! $post->body !!}
+                    </article>
+
+                    <a class="text-secondary-emphasis d-block mt-5" href="/posts">Back to Article</a>
+            </div>
+    </div>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,14 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => "home"
     ]);
 });
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => "about",
         "name" => "Ahmad Ammar Bahtiar",
         "email" => "ammarbahtiarasli@gmail.com",
         "image" => "ahmad.png"
@@ -37,6 +39,7 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function() {
     return view('categories', [
         'title' => 'Post Categories',
+        'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
@@ -44,6 +47,7 @@ Route::get('/categories', function() {
 Route::get('/categories/{category:slug}', function(Category $category) {
     return view('posts', [
         'title' => "Post By Category : $category->name",
+        'active' => 'categories',
         'posts' => $category->posts->load(['category', 'author']),
     ]);
 });
@@ -51,6 +55,7 @@ Route::get('/categories/{category:slug}', function(Category $category) {
 Route::get('authors/{author:username}', function(User $author) {
     return view('posts', [
         'title' => "Post By Author : $author->name",
+        'active' => 'posts',
         'posts' => $author->posts->load(['category', 'author']),
     ]);
 });
